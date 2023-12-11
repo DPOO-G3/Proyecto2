bd3d899e2f6ff213ce7ec731200ed130da4ab31f
Juan Nicolas Suarez Pulido <jn.suarez@uniandes.edu.co>
2023-12-10 22:38:57 -0500

    Cambios Finales

--- src/pruebas/AdmistradorLocalTest.java
+++ src/pruebas/AdmistradorLocalTest.java
@@ -1,7 +1,7 @@
 package pruebas;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import logica.AdministradorLocal;
